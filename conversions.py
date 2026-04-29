7692d4a701c4827eb7929b1c39e202841c9bc051
Gabriel <gabrielcarlosj@gmail.com>
2026-04-28 21:37:20 -0300

    adicionando contornos de bairros

--- conversions.py
+++ conversions.py
@@ -3,25 +3,21 @@
 # VISTO QUE USAMOS ESSES ARQUIVOS NO APLICATIVO CRIADO
 
 
-# pip install geopandas
+# TEM Q DAR pip install geopandas
 import geopandas as gpd
 import requests
 import zipfile
 import io
-import os
 
 url = "https://geoservicos.pbh.gov.br/geoserver/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=ide_bhgeo:BAIRRO_OFICIAL&srsName=EPSG:31983&outputFormat=SHAPE-ZIP"
-
 headers = {
     'User-Agent': 'Mozilla/5.0'
 }
 
 response = requests.get(url, headers=headers)
 
-# Verifica se é realmente um ZIP
 if response.content[:2] != b'PK':
-    print("❌ Não é um arquivo ZIP. Conteúdo recebido:")
-    print(response.text[:500])
+    print(response.text)
 else:
     z = zipfile.ZipFile(io.BytesIO(response.content))
     z.extractall('./data')
